029603d68df9ef0d132f3c80e3f6c12f8901d05c
Jay-Sin <jay@DESKTOP-0Q9HLUV>
2024-03-08 00:27:20 -0500

    move secret to .env

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -7,7 +7,7 @@ import { api } from "@/convex/_generated/api";
 const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 const liveblocks = new Liveblocks({
-    secret: "sk_dev_z7oQ4gIpX6XXd7Lgw7kQJq7500GPiLiNvMGIyIHAdoQZG8jf6dCdVOF0rINnMpUA",
+    secret: process.env.LIVEBLOCKS_SECRET_KEY!,
 });
 
 export async function POST(request: Request) {
